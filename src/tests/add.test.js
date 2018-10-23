b43d5157f253b77d6068d421d4f41c9b14b3ffce
Norayr Ghukasyan <norayr.ghukasian@gmail.com>
2018-10-23 19:07:05 +0400

    add test cases

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,28 +1,21 @@
-const add = (a,b) => a+b;
-const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
+const add = (a,b) => a+ b;
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`
 
 test('should add two numbers', () => {
-    const result = add(3, 4);
+  const result = add(3,4);
 
-    expect(result).toBe(7);
-
-    // if (result !== 7) {
-    //     throw new Error(`You added 4 and 3 . The result was ${result}. Expect 7`);
-    // }
+  expect(result).toBe(7);
 });
 
-test('should generate greeting from name', () => {
-    const result = generateGreeting('Noro');
+test('should generate greeting for name', () => {
+  const result = generateGreeting('Noro');
 
-    expect(result).toBe('Hello Noro!');
+  expect(result).toBe(`Hello Noro!`);
 });
 
-test('should generate greeting from no name', () => {
-    const result = generateGreeting();
+test('should generate greeting for anonymous', () => {
+  const result = generateGreeting();
 
-    expect(result).toBe('Hello Anonymous!');
-});
-
-
-
+  expect(result).toBe(`Hello Anonymous!`);
+})
 
