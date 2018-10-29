2b97ac70321f3b5e903a782c487b450ed2fd8125
Norayr Ghukasyan <norayr.ghukasian@gmail.com>
2018-10-29 10:34:36 +0400

    add tests

---
+++ src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExpenseForm from '../../components/ExpenseForm';
+import expenses from '../fixtures/expenses';
+
+test('should render ExpenseForm correctly', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseForm correctly with expennse data', () => {
+  const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should render error for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {  }
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(0).simulate('change', {
+    target: { value: 'hello' }
+  });
+  expect(wrapper.state('description')).toBe('hello');
+});
