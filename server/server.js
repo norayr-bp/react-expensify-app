279148e71c1f06a3ec924899134eca110c85b76e
Norayr <norayr.ghukasian@gmail.com>
2018-10-31 01:10:17 +0400

    test heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 5000;
 
 app.use(express.static(publicPath));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
